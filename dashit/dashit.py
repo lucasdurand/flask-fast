9d02c59b436a13b7f3b763c0830e6459786df906
Lucas Durand <lucas.durand@gmail.com>
2020-04-04 19:58:52 -0400

    Revamp to focus on Flask. Swagger is happier these days.

---
+++ dashit/dashit.py
@@ -0,0 +1,196 @@
+from typing import List, Callable, Union, Tuple, Any, Dict
+
+from functools import partial
+import inspect
+import pandas as pd
+
+import flask
+import json
+
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+from . import swagger
+
+class DashitError(Exception):
+    pass
+
+
+def generate_rule(f: Callable, app: Union[dash.Dash, flask.Flask]):
+    """
+    Generate Flask rule to map URL positional and query params to function inputs
+    """
+
+    # Pull base url from either Flask or Dash app config.
+    APP_BASE = app.config.get("url_base_pathname") or app.config.get("APPLICATION_ROOT", "/")
+    ENDPOINT = f.__name__
+
+    positional, _, _, _ = parse_args(f)
+    url_args = "".join([f"/<{arg}>" for arg in positional])
+
+    base = f"{APP_BASE}{ENDPOINT}"
+    rule = f"{base}{url_args}"
+    return rule
+
+
+def parse_args(f: Callable):
+
+    argspec = inspect.getfullargspec(f)
+    args = argspec.args
+    defaults = argspec.defaults
+    positional, named = (
+        args[: -len(defaults) if defaults else None],
+        args[-len(defaults) if defaults else None :],
+    )
+    types = argspec.annotations
+    return positional, named, defaults, types
+
+
+def whats_the_url(
+    func: Callable, app: Union[dash.Dash, flask.Flask], *args, **kwargs
+) -> str:
+    """Generate the url to GET the function once you `dashit`"""
+
+    signature = inspect.signature(func)
+    call_errors = ["missing a required argument", "unexpected keyword argument"]
+    try:
+        arguments = signature.bind(*args, **kwargs).arguments
+    except TypeError as e:
+        if any([error in str(e) for error in call_errors]):
+            msg = f"{e}. {func.__name__} is called like {func.__name__}{signature}"
+            raise DashitError(msg) from e
+        else:
+            raise
+
+    args = arguments.pop("args", [])
+    kwargs = arguments.pop("kwargs", {})
+    url = rule = generate_rule(func, app)
+    print(args, kwargs, rule)
+    # postional args
+    for arg, val in arguments.items():
+        url = url.replace(f"<{arg}>", json.dumps(val))
+    # query params
+    url += "?"
+    url += "&".join([f"{keyword}={str(val)}" for keyword, val in kwargs.items()])
+    url = url.strip("?")
+
+    return url
+
+
+def eval_params(params) -> Dict[str, Any]:
+    """Attempt to evaluate dictionary values as python code, else return them as is"""
+
+    kwargs = {}
+    for k, v in params:
+        try:
+            kwargs[k] = json.loads(v)
+        except:
+            kwargs[k] = v
+    return kwargs
+
+
+def inject_flask_params_as_kwargs(func, **kwargs):
+    """Flask converts the url variables into kwargs. We pass those back to our function as args. We also want to inject as kwargs the optional URL parameters"""
+    print(kwargs)
+    positional = eval_params(kwargs.items())
+    print(positional)
+    params = (
+        eval_params(flask.request.args.to_dict(flat=True).items())
+    )  # &var1=["this"]&another=true -> {"var":["this"],"another":"true"}
+
+    call_errors = ["missing a required argument", "unexpected keyword argument"]
+
+    try:
+        value = func(**positional, **params)
+
+    except TypeError as e:
+        if any([error in str(e) for error in call_errors]):
+            signature = inspect.signature(func)
+            msg = f"{func.__name__} is called like {func.__name__}{signature}. {e}."
+            raise DashitError(msg) from e
+        else:
+            raise
+
+    return value
+
+
+def add_rule(app: Union[flask.Flask, dash.Dash], f: Callable):
+    """
+    Register function as Flask route. Positional and named arguments are all required. 
+    In order to handle optional arguments, use **kwargs. *args is right out.
+    """
+    rule = generate_rule(f, app)
+    server = app if isinstance(app, flask.Flask) else app.server
+    view_func = partial(_all_the_small_things, function=f)
+    view_func.__doc__ = f.__doc__
+    server.add_url_rule(
+        rule, endpoint=rule, view_func=view_func
+    )
+    f.url = partial(whats_the_url, f, server)
+    return rule
+
+
+def handle_wacky_types(thing):
+    if isinstance(thing, pd.DataFrame):
+        thing = thing.to_json(orient="table")
+    return thing
+
+
+def _all_the_small_things(function, **kwargs):
+    response = inject_flask_params_as_kwargs(function, **kwargs)
+    response = handle_wacky_types(response)
+    return response
+
+def flaskit(functions: List[Callable], appname:str, version:str="1.0.0") -> flask.Flask:
+    app = flask.Flask(__name__)
+    spec = swagger.create_apispec(appname=appname, version=version)
+    paths = [swagger.register_swag(app=app, spec=spec, func=func, path=add_rule(app=app, f=func)) for func in functions]
+    app = swagger.register_swagger_endpoints(app, spec)
+    print(paths)
+    return app
+
+def dashit(functions: List[Callable], appname: str):
+    """
+    Create a QUICK Dash app exposing your functions as API endpoints!
+    
+    endpoint url:  
+    
+    All positional arguments will be required in the url. Named and keyword args are passed as query params.
+    """
+
+    app = dash.Dash(__name__, url_base_pathname=f"/{appname}/")
+
+    new_routes = [
+        {
+            "name": func.__name__,
+            "docstring": inspect.cleandoc(func.__doc__) if func.__doc__ else "",
+            "endpoint": add_rule(app, func),
+        }
+        for func in functions
+    ]
+
+    # def generate_endpoint_html(route):
+    #     return (
+    #         dcc.Markdown(
+    #             [f"""**{route["name"]}:** [{route["endpoint"]}]({route["endpoint"]})"""]
+    #         ),
+    #         html.Blockquote([dcc.Markdown([route["docstring"]])]),
+    #     )
+
+    # flatten = lambda l: [item for sublist in l for item in sublist]
+
+    # app.layout = html.Div(
+    #     [
+    #         html.H1([appname]),
+    #         html.H3(
+    #             ["DASHIT: Functions Are APIs Now."]
+    #         ),  # some explanation on how the app works. Use function docstring.
+    #         html.Div(flatten([generate_endpoint_html(route) for route in new_routes])),
+    #     ]
+    # )
+    print(new_routes)
+
+    app.layout = html.Div()
+
+    return app
--- dashit/swagger.py
+++ dashit/swagger.py
@@ -1,82 +1,169 @@
 from apispec import APISpec
-from apispec_webframeworks.flask import FlaskPlugin
-import flask
-import yaml
+
+# from apispec_webframeworks.flask import FlaskPlugin
+import flask, dash
+import yaml, json
 from flask import Flask
-from typing import List, Callable, Dict
+from typing import List, Callable, Dict, Union
 import inspect
 
-from . import parse_args
+from . import dashit
 
 
 def create_apispec(appname: str, version: str = "1.0.0") -> APISpec:
     # Create an APISpec
-    spec = APISpec(
-        title=appname,
-        version=version,
-        openapi_version="3.0.2",
-        plugins=[FlaskPlugin()],
-    )
+    spec = APISpec(title=appname, version=version, openapi_version="3.0.3")
     return spec
 
 
-def swagger_format(param_in, name, description, type=None, default=None, **other_stuff):
+def swagger_format(param_in, name, type=None, default=None, **other_stuff):
     param = {
         "in": param_in,
         "name": name,
-        "schema": {"type": type, "default": default},
+        "schema": {"type": type, **({"default": default} if default else {})},
         **other_stuff,
     }
     return param
 
 
+def process_type_name(param_type, default=None):
+    """Allowed types: array, boolean, integer, number, object, string"""
+
+    types = {
+        "list": "array",
+        "dict": "object",
+        "str": "string",
+        "bool": "boolean",
+        "int": "integer",
+        "float": "number",
+        "tuple": "array",
+        "it's complicated": "object",
+    }
+
+    if not param_type and default:  # assume the type of the default value
+        name = type(default).__name__
+    elif not param_type:
+        name = None
+    else:
+        try:  # is this a native python type?
+            name = param_type.__name__
+        except AttributeError:  # ah, so it's from typing?
+            name = (
+                param_type.__origin__
+                if param_type and param_type.__origin__ in types
+                else "it's complicated"
+            )
+
+    js_name = types.get(name, "")
+
+    return js_name
+
+
 def swagger_params(function: Callable, **yet_other_stuff):
-    positional, named, defaults, types = parse_args(function)
+    positional, named, defaults, types = dashit.parse_args(function)
     print(types, defaults, positional, named)
     path_args = [
         swagger_format(
-            param_in="path", name=arg, description=arg, type=types.get(arg, "")
+            param_in="path", name=arg, type=process_type_name(types.get(arg)),
         )
         for arg in positional
     ]
-    query_args = [
-        swagger_format(
-            param_in="query",
-            name=arg,
-            description=arg,
-            type=types.get(arg, ""),
-            default=defaults.get(arg) if defaults else None,
-        )
-        for arg in named
-    ]
+    query_args = (
+        [
+            swagger_format(
+                param_in="query",
+                name=arg,
+                type=process_type_name(types.get(arg), default),
+                default=default,
+            )
+            for default, arg in zip(defaults, named)
+        ]
+        if defaults and named
+        else []
+    )
 
     return path_args + query_args
 
 
-def jigger_the_docstring(function: Callable):
-    docstring = function.__doc__
+def register_swag(app, spec, func, path):
+    docstring = func.__doc__ if func.__doc__ else ""
 
-    if "---" in docstring:
-        return docstring
+    spec.path(
+        path=path.replace("<", "{").replace(">", "}"),
+        operations={
+            "get": {
+                "responses": {
+                    "200": {  # TODO: add info on expected return type
+                        "description": "Success!"
+                    },
+                },
+                "summary": docstring.split("\n")[0],
+                "description": docstring,
+            }
+        },
+        parameters=swagger_params(func),
+    )
+    return path
 
-    name = function.__name__
 
-    swaggered = {
-        "get": {
-            "sumary": f"Why not {name}?",
-            "description": docstring,
-            "parameters": swagger_params(function),
-        }
-    }
-
-    swagger_docstring = "\n---\n" + yaml.dump(swaggered)
-    return docstring + swagger_docstring
-
-
-def register_function_specs(app: flask.Flask, spec: APISpec):
+def register_function_spec(app: Union[flask.Flask, dash.Dash], spec: APISpec):
     # Register the path and the entities within it
-    view_functions = app.view_functions
-    with app.test_request_context():
-        for function in view_functions.values():
-            function.__doc__ = jigger_the_docstring(function)
-            spec.path(view=function)
+    APP_BASE = app.config.get("url_base_pathname") or app.config.get(
+        "APPLICATION_ROOT", "/"
+    )
+    server = app if isinstance(app, flask.Flask) else app.server
+    view_functions = server.view_functions
+    # print(view_functions)
+    for name, function in view_functions.items():
+        if name[0] != "_" and f"{APP_BASE}_" not in name:
+            with server.test_request_context():
+                spec.path(view=function)
+    return server
+
+
+def register_swagger_endpoints(app: Union[flask.Flask, dash.Dash], spec: APISpec):
+    APP_BASE = app.config.get("url_base_pathname") or app.config.get(
+        "APPLICATION_ROOT", "/"
+    )
+    server = app if isinstance(app, flask.Flask) else app.server
+    swagger_endpoint = f"""{APP_BASE}swagger"""
+
+    swagger_ui = f"""<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Swagger</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="//unpkg.com/swagger-ui-dist@3/swagger-ui.css" />
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+        <script src="//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
+        <script>
+        const ui = SwaggerUIBundle({{
+            url: "{swagger_endpoint}",
+            dom_id: '#swagger-ui',
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIBundle.SwaggerUIStandalonePreset
+            ],
+            layout: "BaseLayout",
+            requestInterceptor: (request) => {{
+              request.headers['X-CSRFToken'] = "{{{{ csrf_token }}}}"
+              return request;
+            }}
+          }})
+        </script>
+      </body>
+    </html>"""
+
+    @server.route(f"{APP_BASE}swagger_ui")
+    def serve_swagger_ui():
+        return swagger_ui
+
+    @server.route(swagger_endpoint)
+    def serve_swagger_spec():
+        return json.dumps(spec.to_dict())
+
+    print("Swagger at: " + swagger_endpoint)
+    return server
